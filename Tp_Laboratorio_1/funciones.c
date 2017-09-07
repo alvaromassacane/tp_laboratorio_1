f3da7bb6aacab2e1e6f644b6477b4ea21008a72c
alvaromassacane@yahoo.com.ar <alvaromassacane@yahoo.com.ar>
2017-09-07 19:08:54 -0300

    Trabajo practico 1 laboratorio.

---
+++ Tp_Laboratorio_1/funciones.c
@@ -0,0 +1,55 @@
+
+
+int suma(int num1,int num2)
+{
+    int resultado;
+
+    resultado=num1+num2;
+
+    return resultado;
+}
+
+int resta(int num1,int num2)
+{
+    int resultado;
+
+    resultado=num1-num2;
+
+    return resultado;
+}
+
+
+float division(float num1,float num2)
+{
+    float resultado;
+
+    resultado=num1/num2;
+
+
+
+
+
+        return resultado;
+
+}
+
+int multiplicacion(int num1,int num2)
+{
+    int resultado;
+
+    resultado=num1*num2;
+
+    return resultado;
+
+}
+
+int factorial (int num1)
+{
+    int resultado;
+
+    if(num1==1)
+        return 1;
+    resultado=num1* factorial(num1-1);
+    return resultado;
+
+}
---
+++ Tp_Laboratorio_1/funciones.h
@@ -0,0 +1,51 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+
+int suma(int num1,int num2);
+/** \brief Esta funcion suma dos numeros enteros.
+ *
+ * \param Numero 1
+ * \param Numero 2
+ * \return Entrega suma de los 2 enteros.
+ *
+ */
+
+int resta(int num1,int num2);
+/** \brief Esta funcion resta dos numeros enteros.
+ *
+ * \param Numero 1
+ * \param Numero 2
+ * \return Entrega la resta de los 2 enteros.
+ *
+ */
+
+float division(float num1,float num2);
+/** \brief Esta funcion divide 2 numeros flotantes.
+ *
+ * \param Numero 1
+ * \param Numero 2
+ * \return Entrega la division de los 2 flotantes.
+ *
+ */
+
+
+int multiplicacion(int num1,int num2);
+
+/** \brief Esta funcion multiplica 2 numeros enteros.
+ *
+ * \param Numero 1
+ * \param Numero 2
+ * \return Entrega la multiplicacion de dos numeros enteros.
+ *
+ */
+int factorial (int num1);
+/** \brief Esta funcion calcula el factorial de el numero ingresado
+ *
+ *
+ * \param Numero 1
+ * \return Retorna el factorial del valor ingresado.
+ *
+ */
+
+#endif // FUNCIONES_H_INCLUDED
